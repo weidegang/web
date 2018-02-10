dfa8f14201ac7ce91dda0bf3e2f78cc002041215
weidegang <33209764+weidegang@users.noreply.github.com>
2018-02-10 22:32:13 +0800

    web.sql

---
+++ db/web.sql
@@ -0,0 +1,31 @@
+drop table if exists ROLE_RIGHT;
+
+drop table if exists SYS_ROLE;
+
+drop table if exists SYS_USER;
+
+create table ROLE_RIGHT
+(
+   ID                   varchar(40) not null,
+   ROLE_CODE            varchar(40),
+   RIGHT_CODE           varchar(40),
+   RIGHT_NAME           varchar(40),
+   primary key (ID)
+);
+
+create table SYS_ROLE
+(
+   ID                   varchar(40) not null,
+   ROLE_CODE            varchar(40),
+   ROLE_NAME            varchar(40),
+   primary key (ID)
+);
+
+create table SYS_USER
+(
+   ID                   varchar(40) not null,
+   LOGIN_CODE           varchar(40),
+   LOGIN_NAME           varchar(40),
+   primary key (ID)
+);
+
